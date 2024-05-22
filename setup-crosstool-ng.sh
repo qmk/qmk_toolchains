0b69d0091ef87d813d25573a8a1abd72eac90d79
Nick Brassel <nick@tzarc.org>
2024-05-22 10:34:03 +1000

    Set up devcontainer to try to debug build issues on GHA.

---
+++ setup-crosstool-ng.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -eEuo pipefail
+
+if [[ -d "$HOME/crosstool-ng-install" ]]; then
+    rm -rf "$HOME/crosstool-ng-install"
+fi
+
+git clone https://github.com/crosstool-ng/crosstool-ng.git "$HOME/crosstool-ng-install"
+
+cd "$HOME/crosstool-ng-install"
+./bootstrap
+./configure --prefix=/usr
+make
+sudo make install
