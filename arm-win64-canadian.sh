acc5e9eec5cb28944aea6e1e786f2da03c6e94e0
Nick Brassel <nick@tzarc.org>
2024-05-25 10:56:37 +1000

    Revert "Move to `/t` for Linux builds to try to avoid length problems when `ct-ng` invokes `bash`."
    This reverts commit 26857b3a3960409036c8c13f104f450b6acb96f5.

--- arm-win64-canadian.sh
+++ arm-win64-canadian.sh
@@ -2,7 +2,7 @@
 # Copyright 2024 Nick Brassel (@tzarc)
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-this_script=$(realpath "${BASH_SOURCE[0]}")
+this_script="$PWD/$(basename ${BASH_SOURCE[0]})"
 script_dir=$(dirname "${this_script}")
 source "${script_dir}/common.bashinc"
 
