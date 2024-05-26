9ad8c501f6e5501e8aa99f648f620bb84fa6b9f6
Nick Brassel <nick@tzarc.org>
2024-05-26 20:49:27 +1000

    gcc12 to ensure it matches build machine capabilities.

--- ubuntu-prereqs.sh
+++ ubuntu-prereqs.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
+sudo apt-get dist-upgrade -y
 sudo apt-get install -y build-essential gperf help2man libtool-bin meson flex bison texinfo gawk libncurses-dev patchelf
