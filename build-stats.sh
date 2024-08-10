2029e02123ffc249a5eba13f4c160dc5d087756d
Nick Brassel <nick@tzarc.org>
2024-08-10 22:54:37 +1000

    Potential speedup, macOS build stats logging fixes.

--- build-stats.sh
+++ build-stats.sh
@@ -3,10 +3,10 @@
 while true; do
     sleep 300
     echo
-    ps xfwau
+    ps xfwau 2>/dev/null || ps -ef
     echo
     df -h
     echo
-    free -h
+    free -h 2>/dev/null || top -l 1 -s 0 | grep PhysMem
     echo
 done
